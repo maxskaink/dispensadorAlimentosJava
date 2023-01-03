37ec193daa648cd3bb78e84f82191c72e1a11710
Miguel Angel Calambas <67810439+maxskaink@users.noreply.github.com>
2023-01-03 17:51:49 -0500

    MaxSkank
    Se ha agregado un hola mundo al main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Hola mundo");
     }
+
+    // estos son cambios solo de prueba por miugel
+
 }
