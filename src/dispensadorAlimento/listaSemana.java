6c5ef97cd47953ede6a41ef4e2eb25a69d81c8f7
santiago1scan <93345838+santiago1scan@users.noreply.github.com>
2023-01-03 18:32:44 -0500

    Quitar agregar alimento lista semana

--- src/dispensadorAlimento/listaSemana.java
+++ src/dispensadorAlimento/listaSemana.java
@@ -1,11 +1,26 @@
 package dispensadorAlimento;
 
+import java.util.ArrayList;
+
 public class listaSemana {
 
     private String descripcion;
-    
+    private ArrayList<Alimento> alimentosRef= new ArrayList<Alimento>();
     public listaSemana( String desc ){
         this.descripcion = desc;
     }
+    public void quitarAlimentos(String nombreAlimento){
+        int i;
+
+        for(i= 0; i< (alimentosRef.size()-1); i++){
+            if(alimentosRef.get(i).getNombre().equals(nombreAlimento)){
+                alimentosRef.remove(i);
+            }
+        }
+    }
+    public void agregarAlimento(Alimento objAlimento){
+        this.alimentosRef.add(objAlimento);
+    }
+
     
 }
