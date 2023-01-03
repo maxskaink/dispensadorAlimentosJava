582e54440dc13561db8dc8d19f9a40157a46206a
Santiago <santiescandon1@gmail.com>
2023-01-03 16:49:38 -0500

    commit 2

--- src/dispensadorAlimento/Administrador.java
+++ src/dispensadorAlimento/Administrador.java
@@ -1,2 +1,20 @@
-package dispensadorAlimento;public class Administrador {
+package dispensadorAlimento;
+
+public class   Administrador  extends Usuario{
+    private String rol;
+    public Administrador(String nom, String cant, int ed, String rol) {
+        super(nom, cant, ed);
+        this.setRol(rol);
+
+    }
+
+
+
+    //GIT AND GETTERS
+    public String getRol() {
+        return rol;
+    }
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
 }
