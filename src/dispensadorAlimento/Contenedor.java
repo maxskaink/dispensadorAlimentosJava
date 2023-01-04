ed18129d486c120dcaf93cb761d9ecb5e0e43ac8
Miguel Angel Calambas <67810439+maxskaink@users.noreply.github.com>
2023-01-03 19:08:48 -0500

    Ultimo cambio

--- src/dispensadorAlimento/Contenedor.java
+++ src/dispensadorAlimento/Contenedor.java
@@ -35,7 +35,6 @@ public class Contenedor {
         }
     }
 
-
     //GET AND SETTER
     public String getNombre() {
         return nombre;
--- src/dispensadorAlimento/administradorAlimentos.java
+++ src/dispensadorAlimento/administradorAlimentos.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 public class administradorAlimentos {
 
     private ArrayList<Contenedor> contenedores= new ArrayList<Contenedor>();
+    private ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
 
     private ArrayList<listaSemana> listasPorSemana = new ArrayList<listaSemana>();
 
     private ArrayList<Alimento> AlimentosDisponibles = new ArrayList<Alimento>();
-    private ArrayList<listaSemana> listaPorSemana = new ArrayList<listaSemana>();
 
     public ArrayList<Contenedor> getContenedores(){
         return contenedores;
